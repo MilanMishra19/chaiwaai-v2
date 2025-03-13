eeeee674832b9554bee6d4e744d1310aff057ef9
MilanMishra19 <mishramilan683@gmail.com>
2025-03-13 21:02:02 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,11 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+    origin: 'https://chaiwaai.onrender.com', // Allow only requests from your frontend
+    methods: 'GET, POST, PUT, DELETE, OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization'
+}));
 
 // Routes
 app.use('/api/blogs', blogRoutes);
